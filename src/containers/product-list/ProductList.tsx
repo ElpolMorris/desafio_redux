a55602e79e674de5485e69ba195170cf9b817f58
Pablo Morales Pinto <70617170+ElpolMorris@users.noreply.github.com>
2021-08-06 20:10:38 -0400

    add btn update create

--- src/containers/product-list/ProductList.tsx
+++ src/containers/product-list/ProductList.tsx
@@ -3,62 +3,70 @@ import { Link } from "react-router-dom"
 import { deleteProductAction } from "../../store/products/actions"
 import { productListSelector } from "../../store/products/selectors"
 
-const ProductList = ()=>{
+const ProductList = () => {
     const productList = useSelector(productListSelector)
     const dispatch = useDispatch()
 
-    const handlerDelete = (id: any)=>{
+    const handlerDelete = (id: any) => {
         dispatch(deleteProductAction(id))
     }
 
     return (
         <div>
-            <h1>ProductList</h1>
+            <h1>ProductList <Link
+                className="btn btn-sm btn-primary"
+                to={`/products/create`}
+            >create</Link></h1>
+
             {productList.length > 0 ? (<table className="table">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Precio</th>
-                                    <th>Nombre</th>
-                                    <th>Descripción</th>
-                                    <th>color</th>
-                                    <th className="text-end">
-                                        Acciones
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {productList.map((p: any) => (
-                                    <tr key={p.id}>
-                                        <td>
-                                            <div className="badge bg-dark">
-                                                {p.id}
-                                            </div>
-                                        </td>
-                                        <td>{p.price}</td>
-                                        <td>{p.name}</td>
-                                        <td>{p.description}</td>
-                                        <td>{p.color}</td>
-                                        <td className="text-end">
-                                            <div className="btn-group">
-                                                <button
-                                                    className="btn btn-sm btn-danger"
-                                                    onClick={() => handlerDelete(p.id)}
-                                                >eliminar</button>
-                                                <Link
-                                                    className="btn btn-sm btn-primary"
-                                                    to={`/products/detail/${p.id}`}
-                                                >detalle</Link>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>) : (
-                                <div className="alert alert-danger">
-                                    No existen elementos
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Precio</th>
+                        <th>Nombre</th>
+                        <th>Descripción</th>
+                        <th>color</th>
+                        <th className="text-end">
+                            Acciones
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {productList.map((p: any) => (
+                        <tr key={p.id}>
+                            <td>
+                                <div className="badge bg-dark">
+                                    {p.id}
                                 </div>
-                            )}
+                            </td>
+                            <td>{p.price}</td>
+                            <td>{p.name}</td>
+                            <td>{p.description}</td>
+                            <td>{p.color}</td>
+                            <td className="text-end">
+                                <div className="btn-group">
+                                    <button
+                                        className="btn btn-sm btn-danger"
+                                        onClick={() => handlerDelete(p.id)}
+                                    >eliminar</button>
+                                    <Link
+                                        className="btn btn-sm btn-primary"
+                                        to={`/products/detail/${p.id}`}
+                                    >detalle</Link>
+                                    <Link
+                                        className="btn btn-sm btn-primary"
+                                        to={`/products/update/${p.id}`}
+                                    >update</Link>
+                                </div>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>) : (
+                <div className="alert alert-danger">
+                    No existen elementos
+                </div>
+            )}
         </div>
     )
 }
